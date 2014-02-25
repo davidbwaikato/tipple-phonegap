f6af45e7c10afb3d72867e9103d9948027b12666
David <David@Dedee.(none)>
2014-02-26 11:46:11 +1300

    Changes after initial testing on Windows Phone 8

--- networked-app/www/js/index.js
+++ networked-app/www/js/index.js
@@ -1,22 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
 var app = {
     // Application Constructor
     initialize: function() {
@@ -33,15 +14,37 @@ var app = {
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
+    // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
-	window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, app.onFileSystemSuccess, app.fail);
+		
+		
+		// http://stackoverflow.com/questions/16705223/windows-phone-screen-height-in-phonegap-app-not-100
+		/*
+		if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+			var msViewportStyle = document.createElement("style");
+
+			msViewportStyle.appendChild(
+				document.createTextNode(
+					"@-ms-viewport{width:auto!important}"
+				)
+			);
+
+			msViewportStyle.appendChild(
+				document.createTextNode(
+					"@-ms-viewport{height:device-height!important}"
+				)
+			);
+
+			document.getElementsByTagName("head")[0].appendChild(msViewportStyle);
+		}
+		*/
+		window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, app.onFileSystemSuccess, app.fail);
 
     },
 
     fail: function(error) {
-	console.error("Phonegap app failure: code=" + error.code + " message=" + error.message);
+		console.error("Tipple (PhoneGap JS) failure: code='" + error.code + "', message=" + error.message);
     },
 
     // Update DOM on a Received Event
@@ -58,82 +61,89 @@ var app = {
     },
 
     resizeMap: function() {
-	$("#map-canvas").height(Math.max(100,$(window).height()-90));// TODO set 
+		var message_height = $('#deviceready').height();
+		
+		var map_height = Math.max(100,$(window).height()-message_height);
+		var map_width  = Math.max(100,$(window).width());
+		
+		console.log("resizeMap() map dimensions = (" + map_width + "," + map_height + ")");
+		$("#map-canvas").width(map_width + "px");
+		$("#map-canvas").height(map_height + "px");
     },
 
 
     onFileSystemSuccess: function(fileSystem) {
         console.log("Filesystem name = " + fileSystem.name);
         console.log("Filesystem root name = " + fileSystem.root.fullPath);
-	app.initializeMap(fileSystem.root.fullPath);
+		app.initializeMap(fileSystem.root.fullPath);
     },
 
     initializeMap: function(rootFullPath) {
-	app.resizeMap();
-	
-	var start_lat  = -37.7886;
-	var start_long =  175.317;
+		app.resizeMap();
 
-	// Create and store Leaflet map
-	//this._map = L.map('map-canvas').setView([-37.760, 175.300], 15);				
-	this._map = L.map('map-canvas').setView([start_lat,start_long], 16);
-	var map = this._map;
+		var start_lat  = -37.7886;
+		var start_long =  175.317;
 
-	// On line map
-	
-	L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-	    maxZoom: 18
-	}).addTo(map);
-	
-	
+		// Create and store Leaflet map
+		//this._map = L.map('map-canvas').setView([-37.760, 175.300], 15);				
+		this._map = L.map('map-canvas').setView([start_lat,start_long], 16);
+		var map = this._map;
 
-	//L.tileLayer(rootFullPath + 'tipple-store/geodata/MapQuest/{z}/{x}/{y}.png', {
-	/*	
-	L.tileLayer('img/MapQuest/{z}/{x}/{y}.png', {
-	    maxZoom: 18
-	}).addTo(map);
-*/
+		// On line map
+
+		L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+			maxZoom: 18
+		}).addTo(map);
 
 
-	var uni_message= "The University of Waikato (... in Maaori: Te far-eh Waananga o Waikato), is a comprehensive university in Hamilton, New Zealand, with a satellite campus located in Tauranga. Established in 1964, it was the first university in New Zealand to be designed from a blank canvas.\n\n Waikato is made up of seven faculties and schools, and had more than 12,500 students enrolled at the end of 2012.";
 
-	var marker_popup = L.popup()
-	    .setContent("<b>The University of Waikato</b><br />Established 1964.");
+		//L.tileLayer(rootFullPath + 'tipple-store/geodata/MapQuest/{z}/{x}/{y}.png', {
+		/*	
+		L.tileLayer('img/MapQuest/{z}/{x}/{y}.png', {
+			maxZoom: 18
+		}).addTo(map);
+	*/
 
-	//    .setContent("<b>Hello world!</b><br />I am a popup.");
 
+		var uni_message= "The University of Waikato (... in Maaori: Te far-eh Waananga o Waikato), is a comprehensive university in Hamilton, New Zealand, with a satellite campus located in Tauranga. Established in 1964, it was the first university in New Zealand to be designed from a blank canvas.\n\n Waikato is made up of seven faculties and schools, and had more than 12,500 students enrolled at the end of 2012.";
+
+		var marker_popup = L.popup()
+			.setContent("<b>The University of Waikato</b><br />Established 1964.");
+
+		//    .setContent("<b>Hello world!</b><br />I am a popup.");
+
+				
+		// Marker location within Uni
+		var marker = L.marker([start_lat, start_long])
+			.addTo(map)
+			.bindPopup(marker_popup);
+
+		function onMarkerClick(e) {
+			app.speakToMe(uni_message);
+		}
+		marker.on('click', onMarkerClick);
+
+		function onMarkerPopupClose(e) {
+			speechSynthesis.cancel();
+		}
+		marker_popup.on('popupclosed',onMarkerPopupClose);
+		//map.on('popupclose',onMarkerPopupClose);
+
+		// Dynamic popup on map where you click/touch
+
+		var popup = L.popup();
+		
+		function onMapClick(e) {
+			popup.setLatLng(e.latlng)
+			.setContent("You clicked the map at " + e.latlng.toString())
+			.openOn(map);
 			
-	// Marker location within Uni
-	var marker = L.marker([start_lat, start_long])
-	    .addTo(map)
-	    .bindPopup(marker_popup);
+			//app.speakToMe("You clicked the map at " + e.latlng.toString());
+		}
+		
+		map.on('click', onMapClick);
 
-	function onMarkerClick(e) {
-	    app.speakToMe(uni_message);
-	}
-	marker.on('click', onMarkerClick);
-
-	function onMarkerPopupClose(e) {
-	    speechSynthesis.cancel();
-	}
-	marker_popup.on('popupclosed',onMarkerPopupClose);
-	//map.on('popupclose',onMarkerPopupClose);
-
-	// Dynamic popup on map where you click/touch
-
-	var popup = L.popup();
-	
-	function onMapClick(e) {
-	    popup.setLatLng(e.latlng)
-		.setContent("You clicked the map at " + e.latlng.toString())
-		.openOn(map);
-	    
-	    //app.speakToMe("You clicked the map at " + e.latlng.toString());
-	}
-	
-	map.on('click', onMapClick);
-
-	app.displayCurrentPosition();
+		app.displayCurrentPosition();
 
     },
 
